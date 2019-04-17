9eb0e24ebedf8f4bedb7fa5091bc10a6da484948
Chris Small <chrisasmall+github@gmail.com>
2019-04-17 23:07:51 +0100

    Veinly trying to get a tags page working

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -6,12 +6,46 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
+const PostItem = (node) => {
+  const title = node.frontmatter.title || node.fields.slug
+  return (
+    <div key={node.fields.slug}>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}
+      >
+        <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+          {title}
+        </Link>
+      </h3>
+      <small>{node.frontmatter.date}</small>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: node.frontmatter.description || node.excerpt,
+        }}
+      />
+    </div>
+  )
+}
+
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
-
+    
+    var sortedNodes = new Map();
+    posts.forEach(post => {
+      post.node.fields.tags.forEach(tag => {
+        if (!sortedNodes.get(tag))
+        {
+          sortedNodes.set(tag, new Array());
+        }
+        sortedNodes.set(tag, post.node);
+      });
+    });
+    let i = 0;
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
@@ -19,27 +53,9 @@ class BlogIndex extends React.Component {
           keywords={[`c#`, `.net`, `devops`, `docker`]}
         />
         <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
+        {sortedNodes.forEach((value, key, map) => 
+        {
+          <div><h2>{key}</h2></div>
         })}
 
         <div style={{
@@ -62,14 +78,15 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      limit: 5
+      sort: { fields: [frontmatter___tags], order: DESC }
+      limit: 1000
     ) {
       edges {
         node {
           excerpt
           fields {
             slug
+            tags
           }
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
